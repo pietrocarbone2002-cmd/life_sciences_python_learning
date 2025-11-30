7492246460c13aea331934dff035b5812e0070a6
pietrocarbone2002-cmd <pietrocarbone2002@gmail.com>
2025-11-30 12:35:14 +0100

    Code finished

--- OOP_Basics/restriction_enzymes.py
+++ OOP_Basics/restriction_enzymes.py
@@ -51,7 +51,7 @@ class RestictionEnzyme():
     def __init__(self, name, recognition_site, cut_position):
         self.name = name
         self.recognition_site = recognition_site
-        self.cut_position = int(cut_position)
+        self.cut_position = cut_position
       
     def find_sites(self, dna_sequence):
         start_positions = []
@@ -62,6 +62,61 @@ class RestictionEnzyme():
     
     def cut(self, dna_sequence):
         dna_fragments = []
+        cut_positions = [0]
         for x in range(0, len(dna_sequence) - len(self.recognition_site) + 1):
-            if dna_sequence[x : x + len(self.recognition_site)] == self.recognition_site:
-                
+            if dna_sequence[x : x + len(self.recognition_site)] == self.recognition_site:    
+               cut_positions.append(x + self.cut_position)
+        cut_positions.append(len(dna_sequence))
+
+        for i in range(len(cut_positions) -1):
+            slice = dna_sequence[cut_positions[i] : cut_positions[i + 1]]
+            dna_fragments.append(slice)
+        return dna_fragments
+    
+    def __str__(self):
+        return f"{self.name} ({self.recognition_site}), cuts after position: {self.cut_position}"
+    
+
+restriction_enzymes = {
+    "EcoRI":    {"recognition_site": "GAATTC",   "cut_position": 1},
+    "BamHI":    {"recognition_site": "GGATCC",   "cut_position": 1},
+    "HindIII":  {"recognition_site": "AAGCTT",   "cut_position": 1},
+    "NotI":     {"recognition_site": "GCGGCCGC", "cut_position": 2},
+    "XhoI":     {"recognition_site": "CTCGAG",   "cut_position": 1}
+}
+
+valid_seq_inputs = {"A", "T", "C", "G"}
+
+inp    = input("Enter your restriction enzyme: ")
+inp2   = input("Enter your DNA sequence: ")
+length = len(inp2)
+seq    = str.upper(inp2)
+
+valid_seq = True
+
+if length == 0:
+    valid_seq = False 
+else:
+    for a in range(0, length):
+        if seq[a] not in valid_seq_inputs:
+            valid_seq = False
+            break
+
+if valid_seq:
+    seq1 = RestictionEnzyme(inp,
+                             restriction_enzymes[inp]["recognition_site"],
+                             restriction_enzymes[inp]["cut_position"]
+    )
+    print(f'''
+          {seq1.__str__()}
+          Entered Sequence: {seq}
+          Generated Fragments: {seq1.cut(seq)}
+          Sites of interest fount at position(s): {seq1.find_sites(seq)}
+          ''')
+else:
+    print("Please enter a valid sequence!")   
+
+
+
+
+                                 
