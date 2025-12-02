13a935900f3121b98679ffbe4703969b319bb197
pietrocarbone2002-cmd <pietrocarbone2002@gmail.com>
2025-12-02 15:57:16 +0100

    Exercise Completed

--- Modules/Numpy & Panda/intro.py
+++ Modules/Numpy & Panda/intro.py
@@ -29,7 +29,6 @@ Rules:
 - The DNA sequence can be a hardcoded string (e.g. "ACGTTGCAACGT").
 
 Bonus (optional):
-- Use NumPy's vectorized operations to compute GC-content without loops.
 - Plot the numeric sequence as a line plot using matplotlib.
 
 Objective:
@@ -39,6 +38,7 @@ Understand how NumPy handles numerical arrays and how Pandas organizes biologica
 #Import the required modules
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 #Here we create our DNA-Sequence
 
@@ -52,35 +52,62 @@ encoding = {
     "T":3
 }
 
-length = np.size(seq_list)
 encoded_array = np.array([encoding[base] for base in seq])
+#---------------- NUMPY ----------------
 
-#Python Logic for "Count of each nucleotide"
+#Python Version: Length of the sequence
+length = len(seq)
+
+print(length)
+
+#Numpy Version: Length of the sequence
+length_numpy = np.size(seq_list)
+
+print(length_numpy)
+print("")
+#---------------------------
+
+#Python Version: Count each nucleotide
 counts = {base: 0 for base in encoding.keys()}
 for base in seq:
     counts[base] += 1
 
-# print(counts)
+print(counts)
 
-#Numpy-Version
+#Numpy Version: Count each nucleotide
 unique, counts = np.unique(encoded_array, return_counts=True)
 nucleotides_count = {base: int(count) for base, count in zip(encoding.keys(), counts)}
 
-# print(nucleotides_count)
+print(nucleotides_count)
+print("")
+#---------------------------
 
 #GC-Content Python
 gc = seq.count("G") + seq.count("C")
 gc_percentage = ((gc)/len(seq)) * 100
 
-# print(f'{gc_percentage}%')
+print(f'{gc_percentage}%')
 
 #GC-Content Numpy
 gc_mask = np.isin(encoded_array, [1,2])
 gc_percentage_numpy = gc_mask.mean() * 100
 
-# print(f'{gc_percentage_numpy}%')
+print(f'{gc_percentage_numpy}%')
+print("")
+#---------------- PANDAS ----------------
 
+positions = np.arange(len(seq))
 
+dataframe = pd.DataFrame({
+    "Position":positions,
+    "Base": seq_list,
+    "Numeric": encoded_array
+})
 
+#dataframe.to_csv("sequence_table.csv", index=False)
+print(dataframe)
 
+#---------------- BONUS: MATPLOTLIB ----------------
 
+plot = plt.plot(positions, encoded_array)
+plt.show()
