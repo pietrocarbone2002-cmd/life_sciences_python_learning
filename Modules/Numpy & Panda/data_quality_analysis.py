9acf4fb780e8da5b4c947dd85c4a9da30b89d001
pietrocarbone2002-cmd <pietrocarbone2002@gmail.com>
2025-12-13 21:00:36 +0100

    Data and Dataset created

--- Modules/Numpy & Panda/data_quality_analysis.py
+++ Modules/Numpy & Panda/data_quality_analysis.py
@@ -89,3 +89,27 @@ including inspection, cleaning, transformation, and basic statistical reasoning.
 
 This exercise focuses on thinking in arrays and tables rather than loops.
 '''
+
+import numpy as nu
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Datasets and Dataframe
+
+time = [
+    0, 2, 5, 9, 12, 15, 18, 22, 25, 28,
+    32, 36, 40, 45, 50, 55, 60, 66, 72
+]
+
+measurement = [
+    1.02, 1.05, 1.10, None, 1.25, 1.30, 1.28,
+    1.35, 1.40, 4.90, 1.45, None, 1.55,
+    1.60, 1.62, 1.65, 1.70, 1.72, 1.75
+]
+
+dataframe = pd.DataFrame({
+    "Time" : time,
+    "Measurements" : measurement
+})
+
+print(dataframe)
