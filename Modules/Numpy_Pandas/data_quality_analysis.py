2e538e5e4ec1dfa74f6a7f336ec9cbb2bf4780c5
pietrocarbone2002-cmd <pietrocarbone2002@gmail.com>
2025-12-25 21:44:01 +0100

    Filling implemented

--- Modules/Numpy_Pandas/data_quality_analysis.py
+++ Modules/Numpy_Pandas/data_quality_analysis.py
@@ -142,6 +142,18 @@ print(f'''Missing Values:
 
 #Idea 1: Filling
 
+#Filling time
+
+for a in time:
+   missing_time = (time[a-1] + time[a+1])
+   if time[a] == None:
+      time[a] = missing_time
+
+for b in measurement:
+   k, j = nu.polyfit(time, measurement, 1)
+   missing_value = time[b] * j
+   if measurement[b] == None:
+      measurement = missing_value
 #Implement a mean that fills the missing time: t = (t-1 + t+1)/2
 #Implement a statistical measurement which allows to fill missing values for the measurements
 
