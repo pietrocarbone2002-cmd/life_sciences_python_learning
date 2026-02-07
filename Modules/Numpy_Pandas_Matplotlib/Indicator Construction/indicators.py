b17817973b34d3be7c51b4740c1b089dfc4fda20
pietrocarbone2002-cmd <pietrocarbone2002@gmail.com>
2026-02-07 20:52:55 +0100

    Fast MA implemented

--- Modules/Numpy_Pandas_Matplotlib/Indicator Construction/indicators.py
+++ Modules/Numpy_Pandas_Matplotlib/Indicator Construction/indicators.py
@@ -12,23 +12,40 @@ dataframe = pd.DataFrame(data)
 
 sl_length = 10
 
-
 slow_ma_values = []
-moving_sum = sum(data["Value"][:sl_length])
-slow_ma_values.append(moving_sum/sl_length)
-for i in range(len(data["Value"]) - sl_length):
-    moving_sum += (data["Value"][i + sl_length] - data["Value"][i])
-    slow_ma_values.append(moving_sum/sl_length)
+
+for i in range(len(data["Value"])):
+    if i > sl_length - 1:
+        moving_sum = sum(data["Value"][:sl_length])
+        slow_ma_values.append(moving_sum/sl_length)
+    else:
+        slow_ma_values.append(nu.nan)
 
 slow_ma = nu.array(slow_ma_values)
 
 data["Slow MA"] = slow_ma
 
-print(data)
 #Fast MA
 
 fa_length = 5
 
+fast_ma_values = []
+
+for i in range(len(data["Value"])):
+    if i > fa_length - 1:
+        moving_sum2 = sum(data["Value"][:fa_length])
+        fast_ma_values.append(moving_sum2/fa_length)
+    else:
+        fast_ma_values.append(nu.nan)
+
+fast_ma = nu.array(fast_ma_values)
+
+data["Fast MA"] = fast_ma
+
+print(data)
+
 #Plotting
 ppl.plot(data["Time"], data["Value"], label = "Data")
+ppl.plot(data["Time"], data["Slow MA"], color = "red")
+ppl.plot(data["Time"], data["Slow MA"], color = "orange")
 ppl.show()
