bf82595786d2fe888b8fd7fcc684df776d8a229b
pietrocarbone2002-cmd <pietrocarbone2002@gmail.com>
2026-02-07 23:29:15 +0100

    MAs implemented

--- Modules/Numpy_Pandas_Matplotlib/Indicator Construction/indicators.py
+++ Modules/Numpy_Pandas_Matplotlib/Indicator Construction/indicators.py
@@ -8,44 +8,34 @@ nu_data = data.to_numpy()
 dataframe = pd.DataFrame(data)
 
 #MAs
-#Slow MA
+#Slow MA - Pandas Logik
 
 sl_length = 10
 
-slow_ma_values = []
+data["Slow MA"] = (
+    data["Value"]
+    .rolling(window=sl_length)
+    .mean()
+)
 
-for i in range(len(data["Value"])):
-    if i > sl_length - 1:
-        moving_sum = sum(data["Value"][:sl_length])
-        slow_ma_values.append(moving_sum/sl_length)
-    else:
-        slow_ma_values.append(nu.nan)
+#Fast MA - Python Logik
 
-slow_ma = nu.array(slow_ma_values)
-
-data["Slow MA"] = slow_ma
-
-#Fast MA
-
-fa_length = 5
+fa_length = 4
 
 fast_ma_values = []
 
 for i in range(len(data["Value"])):
-    if i > fa_length - 1:
-        moving_sum2 = sum(data["Value"][:fa_length])
+    if i >= fa_length - 1:
+        moving_sum2 = sum(data["Value"][i - fa_length + 1:i + 1])
         fast_ma_values.append(moving_sum2/fa_length)
     else:
         fast_ma_values.append(nu.nan)
 
 fast_ma = nu.array(fast_ma_values)
-
 data["Fast MA"] = fast_ma
-
-print(data)
 
 #Plotting
 ppl.plot(data["Time"], data["Value"], label = "Data")
 ppl.plot(data["Time"], data["Slow MA"], color = "red")
-ppl.plot(data["Time"], data["Slow MA"], color = "orange")
+ppl.plot(data["Time"], data["Fast MA"], color = "orange")
 ppl.show()
