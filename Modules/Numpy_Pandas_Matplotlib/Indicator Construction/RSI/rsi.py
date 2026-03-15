9482c5e6c57ef6552b2257596116823b7b278e48
pietrocarbone2002-cmd <pietrocarbone2002@gmail.com>
2026-03-15 18:24:55 +0100

    WIP

--- Modules/Numpy_Pandas_Matplotlib/Indicator Construction/RSI/rsi.py
+++ Modules/Numpy_Pandas_Matplotlib/Indicator Construction/RSI/rsi.py
@@ -7,7 +7,25 @@ data = pd.read_csv(r"Modules\Numpy_Pandas_Matplotlib\Indicator Construction\RSI\
 #Data Inspection ---------------------------------------------------------------------
 
 #Visual Test
-ppl.plot(data["Time"], data["Value"])
-ppl.show()
+# ppl.plot(data["Time"], data["Value"])
+# ppl.show()
 
-#Statistical Test
+#Statistical Test - ADF
+
+#RSI = 100 - 100/(1+RS)
+#RS = +dy/-dy
+data["RoC"] = [
+    result - (result-1) for result in (data["Value"] - data["Value"])
+]
+
+#Create the positive RoC for the numerator
+data["+ ROC"] = [
+    pos for pos in data["RoC"] if pos >= 0.
+    ]
+
+#Create the negative RoC for the denominator
+data["- ROC"] = [
+    neg for neg in data["RoC"] if neg <= 0.
+    ]
+
+print(data)
